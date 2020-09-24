e6e633b145e0c7d8be118d68fa2660bf7ec50312
akshat1032 <akshat.30oct@gmail.com>
2020-09-24 20:28:32 +0530

    Added : Welcome Message Added

---
+++ SnakeAndLadderSimulator.java
@@ -0,0 +1,11 @@
+package com.capgemini.snakeandladdersimulator;
+
+public class SnakeAndLadderSimulator {
+	
+	public static void main(String[] args) {
+		
+		//Welcome Message
+		System.out.println("Welcome to Snake and Ladder Game");
+	}
+
+}
