0fe8cbf98697963cd3f8d0611fa33ffd6e00549c
akshat1032 <akshat.30oct@gmail.com>
2020-09-24 20:38:56 +0530

    Added : Single Player Die Roll Added

--- SnakeAndLadderSimulator.java
+++ SnakeAndLadderSimulator.java
@@ -6,8 +6,15 @@ public class SnakeAndLadderSimulator {
 		
 		//Variable
 		int position = 0;
-				
+		int dieRoll = 0;				
+		
 		System.out.println("Start position : "+position);
+				
+		//Die Rolling
+		while (dieRoll == 0) {
+			
+			dieRoll = (int)(Math.floor(Math.random() * 10) % 7);
+		}
 	}
 
 }
